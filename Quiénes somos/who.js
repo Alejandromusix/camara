f4167c2e723ed4f333a61fa88e618775397a2779
¨Isaias¨ <blackkat751@gmail.com>
2025-10-13 22:34:41 -0400

    employee slider completed

--- Quiénes somos/who.js
+++ Quiénes somos/who.js
@@ -1,4 +1,30 @@
-new Swiper('.cards', {
+ document.addEventListener("DOMContentLoaded", function () {
+  
+ const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      }
+    }
+  })
+  });
+  const newSwiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
   loop: true,
 
   // If we need pagination
@@ -12,4 +38,8 @@ new Swiper('.cards', {
     prevEl: '.swiper-button-prev',
   },
 
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
---
+++ register/register.js
@@ -0,0 +1,8 @@
+let popUp = document.getElementById('modal-inner');
+
+function openPopup(){
+    popUp.classList.add("openPopup")
+}
+function closePopup(){
+    popUp.classList.remove("openPopup")
+}
