9d4785350dc19010190e392afe61b599fa665aae
¨Isaias¨ <blackkat751@gmail.com>
2025-10-15 12:25:39 -0400

    Almost finished

--- Main page/index.js
+++ Main page/index.js
@@ -1,42 +1,20 @@
 const year = document.getElementById("year").textContent = new Date().getFullYear();
 
-let slides = document.querySelectorAll(".image-slider img");
-let slideIndex = 0;
-let intervalId = null;
+document.addEventListener("DOMContentLoaded", function () {
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
 
-document.addEventListener("DOMContentLoaded", initializeSlider)
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-function initializeSlider() {
-  if(slides === 0) return; {
-     showSlides(slideIndex);
-     intervalId = setInterval(() => nextSlide(false), INTERVAL_MS)
-}
-}
-function showSlides(index) {
-    if(index >= slides.length) {
-        slideIndex = 0;
-    }
-    else if(index < 0) {
-         slideIndex = slides.length -1;
-    }
-    
-    SlideIndex = index;
-    slides.forEach(s => s.classList.remove("displaySlide"));
-    slides[slideIndex].classList.add("displaySlide");
- };
-
-function prevSlide(manual = true) {
-   clearInterval(intervalId);
-   slideIndex--;
-   showSlides(slideIndex)
-}
-function nextSlide(manual = true) {
-   clearInterval(intervalId);
-   slideIndex++;
-   showSlides(slideIndex)
-}
-function restartInterval() {
-  if (intervalId) clearInterval(intervalId);
-  intervalId = setInterval(() => nextSlide(false), INTERVAL_MS);
-}
-
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+})
